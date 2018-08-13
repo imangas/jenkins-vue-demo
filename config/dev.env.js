786bd54fd589dcc695052162ff2f4c3fd5553d73
Ignacio Mangas <ignaciomangas@gmail.com>
2018-08-13 12:31:07 +0200

    added env-cmd

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,11 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
+const protocol = process.env.https_enabled == 1 ? 'https': 'http'
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  API_ROOT: JSON.stringify(process.env.API_ROOT) || `"${protocol}://localhost:10000"`,
+  ONLYOFFICE_PROXY: JSON.stringify(process.env.ONLYOFFICE_PROXY) || `"${protocol}://localhost:10000/office/iframe"`,
+  CHAT_ROOT: '',
 })
--- config/test.env.js
+++ config/test.env.js
@@ -1,7 +1,11 @@
 'use strict'
 const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
+const protocol = process.env.https_enabled == 1 ? 'https': 'http'
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+  NODE_ENV: '"testing"',
+  API_ROOT: JSON.stringify(process.env.API_ROOT) || `"${protocol}://api.test.romvo.io"`,
+  ONLYOFFICE_PROXY: JSON.stringify(process.env.ONLYOFFICE_PROXY) || `"${protocol}://api.test.romvo.io/office/iframe"`,
+  CHAT_ROOT: '',
 })
